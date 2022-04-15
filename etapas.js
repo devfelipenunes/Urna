1af62dfd1327ccbe609c0b0dea3b0e8202111e00
Dev Felipe Nunes <devfelipenunes@gmail.com>
2022-04-14 21:54:02 -0300

    atualização

---
+++ etapas.js
@@ -0,0 +1,54 @@
+let etapas = [
+    {
+        titulo: 'GOVERNADOR',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '12',
+                name: 'Muito Cagadinho',
+                vice: 'Cagadinho',
+                partido: 'Roubos Grandes',
+                fotos: [
+                    {url:'candidato.jpg',legenda: 'Governador', small: false},
+                    {url:'candidato.png',legenda: 'Governador', small: true}
+                ]
+            },
+            {
+                numero: '23',
+                name: 'Muito Cagado',
+                vice: 'Cagadão',
+                partido: 'Grandes Roubos',
+                fotos: [
+                    {url:'candidato.jpg', legenda: 'Governador'},
+                    {url:'candidato.png', legenda: 'Governador', small:true},
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '12',
+                name: 'Grande Merda',
+                partido: 'Grandes Roubos',
+                vice: 'Grande Merdinha',
+                fotos: [
+                    {url:'candidato.jpg',legenda: 'Presidente'},
+                    {url:'candidato.png',legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '23',
+                name: 'Merda Grande',
+                partido: 'Roubos Grandes',
+                vice: 'Merdinha Grande',
+                fotos: [
+                    {url:'candidato.jpg', legenda: 'Presidente'},
+                    {url:'candidato.png', legenda: 'Vice-Presidente', small: true}
+                ]
+            }
+        ]
+    },
+]
